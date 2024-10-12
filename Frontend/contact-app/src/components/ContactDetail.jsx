262e1ad1d310bcfc0723d0c319b6025c3248f649
Rimsha-Ajmal <114993374+Rimsha-Ajmal@users.noreply.github.com>
2024-10-11 22:12:43 -0700

    code formating

--- Frontend/contact-app/src/components/ContactDetail.jsx
+++ Frontend/contact-app/src/components/ContactDetail.jsx
@@ -1,128 +1,186 @@
-import { React, useEffect, useRef, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import { getContact, updatePhoto } from '../api/ContactService';
-import { toastError, toastSuccess } from '../api/ToastService';
+import { React, useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getContact, updatePhoto } from "../api/ContactService";
+import { toastError, toastSuccess } from "../api/ToastService";
 
 const ContactDetail = ({ updateContact, updateImage }) => {
+  const inputRef = useRef();
+  const [contact, setContact] = useState({
+    id: "",
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    title: "",
+    status: "",
+    photoUrl: "",
+  });
 
-    const inputRef = useRef();
-    const [contact, setContact] = useState({
-        id: "",
-        name: "",
-        email: "",
-        phone: "",
-        address: "",
-        title: "",
-        status: "",
-        photoUrl: ""
-    });
+  const { id } = useParams();
+  //   console.log(id)
 
-    const { id } = useParams();
-    //   console.log(id)
-
-    const fetchContact = async (id) => {
-        try {
-            const { data } = await getContact(id);
-            // console.log(data);
-            setContact(data);
-            console.log(data);
-            // toastSuccess("Contact retrieved.");
-        } catch (error) {
-            console.log(error);
-            toastError(error.message);
-        }
-    };
-
-    const selectImage = () => {
-        inputRef.current.click();
-    };
-
-    const updatePhoto = async (file) => {
-        try {
-            const formData = new FormData();
-            formData.append('file', file, file.name);
-            formData.append('id', id);
-            await updateImage(formData);
-            setContact((prev)=>({...prev, photoUrl: `${prev.photoUrl}?updated_at=${new Date().getTime()}`}));
-            toastSuccess("Photo updated.");
-        } catch (error) {
-            console.log(error);
-            toastError(error.message);
-        }
-    };
-
-    const onChange = (event) => {
-        setContact({...contact, [event.target.name]: event.target.value});
-        console.log(contact);
+  const fetchContact = async (id) => {
+    try {
+      const { data } = await getContact(id);
+      // console.log(data);
+      setContact(data);
+      console.log(data);
+      // toastSuccess("Contact retrieved.");
+    } catch (error) {
+      console.log(error);
+      toastError(error.message);
     }
+  };
 
-    const onUpdateContact = async (event) => {
-        event.preventDefault();
-        await updateContact(contact);
-        fetchContact(id);
-        toastSuccess("Contact updated.");
+  const selectImage = () => {
+    inputRef.current.click();
+  };
+
+  const updatePhoto = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+      formData.append("id", id);
+      await updateImage(formData);
+      setContact((prev) => ({
+        ...prev,
+        photoUrl: `${prev.photoUrl}?updated_at=${new Date().getTime()}`,
+      }));
+      toastSuccess("Photo updated.");
+    } catch (error) {
+      console.log(error);
+      toastError(error.message);
     }
+  };
 
-    useEffect(() => {
-        fetchContact(id)
-    }, []);
+  const onChange = (event) => {
+    setContact({ ...contact, [event.target.name]: event.target.value });
+    console.log(contact);
+  };
 
-    return (
-        <>
-            <Link to={"/"} className='link'><i className='bi bi-arrow-left'></i> Back to list</Link>
-            <div className='profile'>
-                <div className='profile__details'>
-                    <img src={contact.photoUrl} alt={`Profile photo of ${contact.name}`} />
-                </div>
-                <div className='profile_metadata'>
-                    <p className='profile__name'>{contact.name}</p>
-                    <p className='profile__muted'>JPG, GIF, or PNG. Max size of 10MG</p>
-                    <button onClick={selectImage} className='btn'><i className='bi bi-cloud-upload'></i> Change Photo</button>
-                </div>
-                
-                <div className='profile__settings'>
-                    <div>
-                        <form onSubmit={onUpdateContact} className="form">
-                            <div className="user-details">
-                                <input type="hidden" defaultValue={contact.id} name="id" required />
-                                <div className="input-box">
-                                    <span className="details">Name</span>
-                                    <input type="text" value={contact.name} onChange={onChange} name="name" required />
-                                </div>
-                                <div className="input-box">
-                                    <span className="details">Email</span>
-                                    <input type="text" value={contact.email} onChange={onChange} name="email" required />
-                                </div>
-                                <div className="input-box">
-                                    <span className="details">Phone</span>
-                                    <input type="text" value={contact.phone} onChange={onChange} name="phone" required />
-                                </div>
-                                <div className="input-box">
-                                    <span className="details">Address</span>
-                                    <input type="text" value={contact.address} onChange={onChange} name="address" required />
-                                </div>
-                                <div className="input-box">
-                                    <span className="details">Title</span>
-                                    <input type="text" value={contact.title} onChange={onChange} name="title" required />
-                                </div>
-                                <div className="input-box">
-                                    <span className="details">Status</span>
-                                    <input type="text" value={contact.status} onChange={onChange} name="status" required />
-                                </div>
-                            </div>
-                            <div className="form_footer">
-                                <button type="submit" className="btn">Save</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  const onUpdateContact = async (event) => {
+    event.preventDefault();
+    await updateContact(contact);
+    fetchContact(id);
+    toastSuccess("Contact updated.");
+  };
 
-            <form style={{display:'none'}}>
-                <input type='file' ref={inputRef} onChange={(event) => updatePhoto(event.target.files[0])} name='file' accept='image/*'/>
+  useEffect(() => {
+    fetchContact(id);
+  }, []);
+
+  return (
+    <>
+      <Link to={"/"} className="link">
+        <i className="bi bi-arrow-left"></i> Back to list
+      </Link>
+      <div className="profile">
+        <div className="profile__details">
+          <img
+            src={contact.photoUrl}
+            alt={`Profile photo of ${contact.name}`}
+          />
+        </div>
+        <div className="profile_metadata">
+          <p className="profile__name">{contact.name}</p>
+          <p className="profile__muted">JPG, GIF, or PNG. Max size of 10MG</p>
+          <button onClick={selectImage} className="btn">
+            <i className="bi bi-cloud-upload"></i> Change Photo
+          </button>
+        </div>
+
+        <div className="profile__settings">
+          <div>
+            <form onSubmit={onUpdateContact} className="form">
+              <div className="user-details">
+                <input
+                  type="hidden"
+                  defaultValue={contact.id}
+                  name="id"
+                  required
+                />
+                <div className="input-box">
+                  <span className="details">Name</span>
+                  <input
+                    type="text"
+                    value={contact.name}
+                    onChange={onChange}
+                    name="name"
+                    required
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Email</span>
+                  <input
+                    type="text"
+                    value={contact.email}
+                    onChange={onChange}
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Phone</span>
+                  <input
+                    type="text"
+                    value={contact.phone}
+                    onChange={onChange}
+                    name="phone"
+                    required
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Address</span>
+                  <input
+                    type="text"
+                    value={contact.address}
+                    onChange={onChange}
+                    name="address"
+                    required
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Title</span>
+                  <input
+                    type="text"
+                    value={contact.title}
+                    onChange={onChange}
+                    name="title"
+                    required
+                  />
+                </div>
+                <div className="input-box">
+                  <span className="details">Status</span>
+                  <input
+                    type="text"
+                    value={contact.status}
+                    onChange={onChange}
+                    name="status"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form_footer">
+                <button type="submit" className="btn">
+                  Save
+                </button>
+              </div>
             </form>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
 
-export default ContactDetail
+      <form style={{ display: "none" }}>
+        <input
+          type="file"
+          ref={inputRef}
+          onChange={(event) => updatePhoto(event.target.files[0])}
+          name="file"
+          accept="image/*"
+        />
+      </form>
+    </>
+  );
+};
+
+export default ContactDetail;
